08dcaee48ec278153ed91cbfe3cb616dd5c89b22
Vasiliy Pankrukhin <wacpan93@gmail.com>
2021-11-02 17:47:25 +0300

    решение к дз 3 урока

---
+++ Урок 3/Lesson_3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "Hello World!";
+            Console.WriteLine(Reverse(str));
+        }
+
+        static string Reverse(string str)
+        {
+            var strReversed = "";
+            for (int i = str.Length-1; i>=0; i--)
+            {
+                strReversed += str[i];
+            }
+            return strReversed;
+        }
+    }
+}
---
+++ Урок 3/Lesson_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NewAccount_1 = new Account(100.05m);
+            var NewAccount_2 = new Account(99.99m);
+
+            NewAccount_1.Transfer(NewAccount_2, 50m);
+
+            Console.WriteLine($"Номер счета: {NewAccount_1.num}\nБаланс: {NewAccount_1.balance}");
+            Console.WriteLine($"Номер счета: {NewAccount_2.num}\nБаланс: {NewAccount_2.balance}");
+        }
+    }
+}
