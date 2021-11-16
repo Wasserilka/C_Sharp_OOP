5cbad5930a92bf70a297da67049a83b44be8680d
Vasiliy Pankrukhin <wacpan93@gmail.com>
2021-11-16 17:24:07 +0300

    решение к дз 7 урока

---
+++ Урок 7/Lesson_7/ICoder.cs
@@ -0,0 +1,8 @@
+﻿namespace Lesson_7
+{
+    public interface ICoder
+    {
+        string Encode(string str, Language lang);
+        string Decode(string str, Language lang);
+    }
+}
---
+++ Урок 7/Lesson_7/Language.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    public class Language
+    {
+        private string _name;
+        private int _minLowCase;
+        private int _maxLowCase;
+        private int _minHighCase;
+        private int _maxHighCase;
+        private int _difference;
+
+        public string Name { get { return _name; } }
+        public int MinLowCase { get { return _minLowCase; } }
+        public int MaxLowCase { get { return _maxLowCase; } }
+        public int MinHighCase { get { return _minHighCase; } }
+        public int MaxHighCase { get { return _maxHighCase; } }
+        public int Difference { get { return _difference; } } 
+
+        public Language(string name, int minLowCase, int maxLowCase, int minHighCase, int maxHighCase)
+        {
+            _name = name;
+            _minLowCase = minLowCase;
+            _maxLowCase = maxLowCase;
+            _minHighCase = minHighCase;
+            _maxHighCase = maxHighCase;
+            _difference = maxLowCase - minLowCase;
+        }
+    }
+}
